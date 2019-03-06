cac1a011d8884c607515613916841cd305ed65c1
esteban zambrano <ezam0000@gmail.com>
2019-03-06 22:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 var kittens = ["Milo", "Otis", "Garfield"]
-var lastkitten=Ralph
-var firstkitten=Bob
-// Add your functions and code here
-function destructivelyAppendKitten(Ralph){
-return kittens.push(Ralph)
+
+function destructivelyAppendKitten(element){
+  kittens.push(element)
+  return kittens
 }
-function destructivelyPrependKitten(Bob){
-  return kittens.unshift(Bob)
+function destructivelyPrependKitten(element){
+kittens.unshift(element)
+return kittens
 }
-function destructivelyRemoveLastKitten(name){
-  return kittens.pop()
+function destructivelyRemoveLastKitten (element){
+kittens.pop()
+return kittens
 }
-function destructivelyRemoveFirstKitten(name){
-  return kittens.shift()
+function destructivelyRemoveFirstKitten(element){
+  kittens.shift()
+  return kittens
 }
-function appendKitten(Broom){
-  return [...kittens, Broom]}
-  
-function prependKitten(Arnold){
-  return [Arnold, ...kittens]
+function appendKitten(name){
+  return[...kittens,name]
+}
+function prependKitten(name){
+  return[name,...kittens]
 }
 function removeLastKitten(){
-  return kittens.slice(0, kittens.length -1)
+  return kittens.slice(0,kittens.length,-1)
+ 
 }
 function removeFirstKitten(){
   return kittens.slice(1)
-}
   
+}
